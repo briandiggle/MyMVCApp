1d64db972979ecdfc3292243fd767b61f4fcdf41
Brian Diggle <brian.diggle@callcredit.co.uk>
2014-06-16 15:44:23 +0100

    Create walk now migrated to jQuery 2.1.1 and  jQuery UI 1.10.4

---
+++ MyMVCApp.DAL/UsefulSQLQueries/GetAllDataOnWalk.sql
@@ -0,0 +1,15 @@
+declare @walkid int
+
+set @walkid=319
+
+select max(WalkID) from dbo.Walks
+
+select * from Walk_AssociatedFiles where WalkID=@walkid
+
+select * from HillAscent where WalkID=@walkid
+
+select * from Walks where WalkID=@walkid
+
+select * from Marker where WalkID=@walkid
+
+
